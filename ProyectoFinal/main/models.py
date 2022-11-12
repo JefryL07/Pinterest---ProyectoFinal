10fb68155a77b66d40a6a76e9d5de8794759ef23
Jefry Lezcano <jefry07lezcano@gmail.com>
2022-11-11 22:42:58 -0500

    Actualizar el index y los modelos
    Se añadió el modelo de Usuario
    Se separó el nav y el footer del index

--- ProyectoFinal/main/models.py
+++ ProyectoFinal/main/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class UserProfile(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'User Profiles'
+        verbose_name = 'User Profile'
+    
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.ImageField(blank=True, null=True, upload_to="avatar")
+    title = models.CharField(max_length=200, blank=True, null=True)
+    bio = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.user.first_name} {self.user.last_name}'
